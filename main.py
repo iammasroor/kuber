501f52e5b391501e7f1507fd314b4eb2815750a2
root <root@ip-172-31-82-229.ec2.internal>
2022-06-13 10:37:35 +0000

    Third commit

--- main.py
+++ main.py
@@ -8,3 +8,4 @@ templates = Jinja2Templates(directory="/code")
 @app.get("/")
 def form_post(request: Request):
     return templates.TemplateResponse('form.html', context={'request': request})
+print  (" ")
